b36fb4c6a7c618adc79c65df6cf33c0b6da0900e
shinnosuke-K <shin.050195@gmail.com>
2024-11-10 16:00:13 +0900

    response/request実装

--- backend/pkg/app/presenter/request/request.go
+++ backend/pkg/app/presenter/request/request.go
@@ -1,1 +1,20 @@
 package request
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+)
+
+func Decode[T any](r *http.Request, v *T) error {
+	var (
+		buf = new(bytes.Buffer)
+		tee = io.TeeReader(r.Body, buf)
+	)
+	if err := json.NewDecoder(tee).Decode(v); err != nil {
+		return fmt.Errorf("failed to decode request body:[%s] %w", buf.String(), err)
+	}
+	return nil
+}
--- backend/pkg/app/presenter/response/response.go
+++ backend/pkg/app/presenter/response/response.go
@@ -1,1 +1,30 @@
 package response
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+// OK - 200
+func OK(w http.ResponseWriter, body any) {
+	writeResponseJSON(w, http.StatusOK, body)
+}
+
+// Created - 201
+func Created(w http.ResponseWriter, body any) {
+	writeResponseJSON(w, http.StatusCreated, body)
+}
+
+// NoContent - 204
+func NoContent(w http.ResponseWriter, body any) {
+	writeResponseJSON(w, http.StatusNoContent, body)
+}
+
+func writeResponseJSON(w http.ResponseWriter, statusCode int, body any) {
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+	w.WriteHeader(statusCode)
+	if err := json.NewEncoder(w).Encode(body); err != nil {
+		http.Error(w, fmt.Errorf("failed to encode json. %w", err).Error(), http.StatusInternalServerError)
+	}
+}
