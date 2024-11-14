07929365d3d018ba5362f12194ae64c0271a1ca7
shinnosuke kishida <shin.050195@gmail.com>
2024-11-14 12:54:24 +0900

    python環境を用意 (#5)
    * move director
    
    * change path
    
    * add host
    
    * add python file
    
    * add docker for python
    
    * add environment

--- backend/go/pkg/infrastructure/http/http.go
+++ backend/go/pkg/infrastructure/http/http.go
@@ -1,18 +1,24 @@
 package http
 
 import (
+	"cmp"
 	"context"
 	"net/http"
+	"os"
 
 	appRPC "github.com/auster-kaki/auster-mono/pkg/app/rpc"
 )
 
 type Client struct {
 	client *http.Client
+	host   string
 }
 
 func NewClient() *Client {
-	return &Client{client: http.DefaultClient}
+	return &Client{
+		client: http.DefaultClient,
+		host:   cmp.Or(os.Getenv(""), "localhost"),
+	}
 }
 
 func (c *Client) Diary() appRPC.Diary { return c }
