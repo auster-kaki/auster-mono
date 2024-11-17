12fb9656be438b4a7b0d0c5305092c35d4d912ef
platykun <nagonago561@yahoo.co.jp>
2024-11-17 15:23:27 +0900

    途上コミット

---
+++ frontend/components/c/diary/Diary.vue
@@ -0,0 +1,34 @@
+<!--
+Usage example:
+<diary :diary="selectedDiary" />
+-->
+
+<script setup lang="ts">
+import { defineProps } from 'vue'
+
+const props = defineProps<{
+  diary: Diary | null
+}>()
+
+interface Diary {
+  image: string
+  title: string
+  date: string
+  location: string
+  content: string
+}
+</script>
+
+<template>
+  <v-card v-if="diary">
+    <v-img :src="diary.image" height="300"></v-img>
+    <v-card-title>{{ diary.title }}</v-card-title>
+    <v-card-subtitle
+    >{{ diary.date }} -
+      {{ diary.location }}
+    </v-card-subtitle>
+    <v-card-text>{{ diary.content }}</v-card-text>
+  </v-card>
+</template>
+
+<style scoped></style>
