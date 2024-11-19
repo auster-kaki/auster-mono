de7023ca49f43b83206b6c65e4fc5a73a6445805
shimoe-sot <mamoru.shimoe@so-tech.co.jp>
2024-11-20 00:30:44 +0900

    仕様変更の反映、デザイン一部見直し

--- frontend/components/c/diary/DiaryCarousel.vue
+++ frontend/components/c/diary/DiaryCarousel.vue
@@ -1,98 +1,99 @@
 <template>
   <v-container class="pa-2">
-    <v-carousel
-      max-width="600"
-      height="680"
-      hide-delimiter-background
-      class="elevation-4"
-      style="border-radius: 4px"
-    >
-      <v-carousel-item
-        v-for="entry in diaries"
-        :key="entry.id"
-      >
-        <v-card flat style="position: relative;">
-          <v-container>
-            <v-row class="pa-2 align-center">
-              <div class="custom-button mr-2">
-                {{ entry.date.slice(0, 4) }}<br>
-                {{ entry.date.slice(5).replace('/', '') }}
-              </div>
-              <h3 class="text-center font-weight-bold">{{ entry.title }}</h3>
-              <v-spacer />
-              <v-icon size="24" @click="showBookmarkMessage">mdi-bookmark</v-icon>
-            </v-row>
-            <v-row class="px-2">
-              <v-img
-                :src="require(`@/static/destination/${entry.image}`)"
-                style="border-radius: 4px"
-                :alt="entry.title"
-                cover
-                height="270"
-                width="100%"
-              />
-            </v-row>
-          </v-container>
+    <v-card style="position: relative;" elevation-4>
+      <v-container>
+        <v-row class="pa-2 align-center">
+          <div class="custom-button mr-2">
+            {{ diary.date.slice(0, 4) }}<br>
+            {{ diary.date.slice(5).replace('/', '') }}
+          </div>
+          <h3 class="text-center font-weight-bold">{{ diary.title }}</h3>
+          <v-spacer />
+          <v-icon size="24" @click="showBookmarkMessage">mdi-bookmark</v-icon>
+        </v-row>
+        <v-row class="px-2">
+          <v-img
+            :src="require(`@/static/destination/${diary.image}`)"
+            style="border-radius: 4px"
+            :alt="diary.title"
+            cover
+            height="270"
+            width="100%"
+          />
+        </v-row>
+      </v-container>
 
-          <v-card-text class="text-body-1" style="height:250px; overflow-y: auto;">
-            {{ entry.content }}
-          </v-card-text>
-          <v-sheet class="d-flex justify-center" style="position: relative;">
-            <v-btn
-              color="primary"
-              class="px-4 py-2 mb-2"
-              @click="emit('select', entry.id)"
-            >
-              <span class="text-white font-weight-bold">体験詳細</span>
-            </v-btn>
-            <v-sheet style="position: absolute; top: 0; right: 0;">
-              <v-btn icon @click="showShareMessage">
-                <v-icon size="x-large">mdi-share-variant</v-icon>
-              </v-btn>
-              <v-btn icon class="mr-4" @click="showDownloadMessage">
-                <v-icon size="x-large">mdi-download</v-icon>
-              </v-btn>
-            </v-sheet>
-          </v-sheet>
-        </v-card>
-      </v-carousel-item>
-    </v-carousel>
+      <v-card-text class="text-body-1" style="height:250px; overflow-y: auto;">
+        {{ diary.content }}
+      </v-card-text>
+      <v-sheet class="d-flex justify-center" style="position: relative;">
+        <v-btn
+          color="primary"
+          class="px-4 py-2 mb-2"
+          @click="handleSelect"
+        >
+          <span class="text-white font-weight-bold">体験詳細</span>
+        </v-btn>
+        <v-sheet style="position: absolute; top: 0; right: 0;">
+          <v-btn icon @click="showShareMessage">
+            <v-icon size="x-large">mdi-share-variant</v-icon>
+          </v-btn>
+          <v-btn icon class="mr-4" @click="showDownloadMessage">
+            <v-icon size="x-large">mdi-download</v-icon>
+          </v-btn>
+        </v-sheet>
+      </v-sheet>
+    </v-card>
     <v-snackbar v-model="snackbar" :timeout="2000" color="accent" class="text-center">
       {{ snackbarMessage }}
     </v-snackbar>
   </v-container>
 </template>
 
-<script setup>
+<script>
 import { ref } from 'vue'
 
-defineProps({
-  diaries: {
-    type: Array,
-    required: true
+export default {
+  props: {
+    diary: {
+      type: [Object],
+      required: true
+    }
+  },
+  setup(props, { emit }) {
+    const snackbar = ref(false)
+    const snackbarMessage = ref('')
+
+    const showMessage = (message) => {
+      snackbarMessage.value = message
+      snackbar.value = true
+    }
+
+    const showDownloadMessage = () => {
+      showMessage('日記がダウンロードできる予定です')
+    }
+
+    const showBookmarkMessage = () => {
+      showMessage('ブックマークして後から見返せるようになる予定です')
+    }
+
+    const showShareMessage = () => {
+      showMessage('SNSでシェアできるようになる予定です')
+    }
+
+    const handleSelect = () => {
+      emit('select', props.diary.id)
+    }
+
+    return {
+      snackbar,
+      snackbarMessage,
+      showDownloadMessage,
+      showBookmarkMessage,
+      showShareMessage,
+      handleSelect
+    }
   }
-})
-
-const emit = defineEmits(['select'])
-
-const snackbar = ref(false)
-const snackbarMessage = ref('')
-
-const showMessage = (message) => {
-  snackbarMessage.value = message
-  snackbar.value = true
-}
-
-const showDownloadMessage = () => {
-  showMessage('日記がダウンロードできる予定です')
-}
-
-const showBookmarkMessage = () => {
-  showMessage('ブックマークして後から見返せるようになる予定です')
-}
-
-const showShareMessage = () => {
-  showMessage('SNSでシェアできるようになる予定です')
 }
 </script>
 
--- frontend/pages/c/new-diary/index.vue
+++ frontend/pages/c/new-diary/index.vue
@@ -1,6 +1,6 @@
 <template>
   <v-stepper v-model="currentStep" class="mb-16" flat max-width="1200px" style="margin: 0 auto;">
-    <v-stepper-header>
+    <v-stepper-header style="box-shadow: unset !important;">
       <v-stepper-step :complete="currentStep > 1" step="1" />
       <v-divider />
       <v-stepper-step :complete="currentStep > 2" step="2" />
@@ -23,7 +23,7 @@
           :departure-time="departureForm.departureTime"
           :return-time="departureForm.returnTime"
           :interests="departureForm.interests"
-          @submit="currentStep += 1"
+          @submit="handleDepatureSelectionSubmit"
         />
       </v-stepper-content>
 
@@ -42,23 +42,23 @@
           :video-title="experienceForm.videoTitle"
           :video-description="experienceForm.videoDescription"
           :experiences="experienceForm.experiences"
+          @click="handleSelectExperience"
         />
         <v-container>
           <v-row class="mt-4 pb-4">
             <v-btn text @click="currentStep -= 1">戻る</v-btn>
-            <v-spacer />
-            <v-btn color="primary" @click="handleSelectExperience">この地域にする！</v-btn>
           </v-row>
         </v-container>
       </v-stepper-content>
       <v-stepper-content style="padding: 8px" step="4">
-        <diary-carousel :diaries="diaries" @select="handleSelectDiary" />
+        <diary-carousel :diary="createdDiary" @select="handleSelectDiary" />
         <v-btn text @click="currentStep -= 1">
           戻る
         </v-btn>
       </v-stepper-content>
       <v-stepper-content step="5">
         <NewDiaryItinerary
+          :bring="bring"
           :itinerary="itinerary"
         />
         <v-container>
@@ -131,7 +131,14 @@ export default {
           experiences: []
         }
       },
-      diaries: [],
+      createdDiary: {
+        id: 1,
+        date: '2024/11/23',
+        image: 'choshi.jpg',
+        title: '大物ヒラマサとの出会い',
+        content: '今日は早朝から漁船に乗り、期待に胸を膨らませて出航しました。風は少し冷たかったけれど、海の静けさが心地よかったです。そして、ついに大物のヒラマサがヒット！かなりの引きで、腕がパンパンになりましたが、無事に釣り上げることができました。この魚の力強さと美しさには感動しました。次回もこのサイズを狙いたいと思います！'
+      },
+      bring: [],
       itinerary: [],
       bookingInfo: {
         expressTickets: [],
@@ -148,9 +155,9 @@ export default {
     this.userInfo = userStore.userInfo
   },
   methods: {
-    submitForm() {
-      // Handle form submission
-      console.log('Form submitted')
+    handleDepatureSelectionSubmit(model) {
+      this.departureForm = model
+      this.currentStep += 1
     },
     handleDestinationSelected(id) {
       this.destinationForm.id = id
@@ -191,33 +198,21 @@ export default {
       }
       this.currentStep += 1
     },
-    handleSelectExperience() {
+    handleSelectExperience(id) {
       // TODO 地域情報からこれらの情報取ってくる
-      this.diaries = [
-        {
-          id: 1,
-          date: '2024/11/23',
-          image: 'choshi.jpg',
-          title: '大物ヒラマサとの出会い',
-          content: '今日は早朝から漁船に乗り、期待に胸を膨らませて出航しました。風は少し冷たかったけれど、海の静けさが心地よかったです。そして、ついに大物のヒラマサがヒット！かなりの引きで、腕がパンパンになりましたが、無事に釣り上げることができました。この魚の力強さと美しさには感動しました。次回もこのサイズを狙いたいと思います！'
-        },
-        {
-          id: 2,
-          date: '2024/11/24',
-          image: 'choshi.jpg',
-          title: '穏やかな朝の散歩',
-          content: '今朝は日の出とともに目覚め、近所の公園を散歩しました。紅葉が見頃で、朝日に照らされた葉が美しく輝いていました。静かな朝の時間を過ごすことで、一日を穏やかな気持ちで始められそうです。'
-        },
-        {
-          id: 3,
-          date: '2024/11/25',
-          image: 'choshi.jpg',
-          title: '新しいカフェでの発見',
-          content: '街中にオープンしたという評判のカフェに行ってきました。インテリアがとてもおしゃれで、窓から差し込む光が心地よい空間でした。注文したラテアートが素晴らしく、バリスタの技術に感動。また訪れたい場所が増えました。'
-        }]
+      this.$emit('diary-selected', {
+        id: 1,
+        date: '2024/11/23',
+        image: 'choshi.jpg',
+        title: '大物ヒラマサとの出会い',
+        content: '今日は早朝から漁船に乗り、期待に胸を膨らませて出航しました。風は少し冷たかったけれど、海の静けさが心地よかったです。そして、ついに大物のヒラマサがヒット！かなりの引きで、腕がパンパンになりましたが、無事に釣り上げることができました。この魚の力強さと美しさには感動しました。次回もこのサイズを狙いたいと思います！'
+      })
       this.currentStep += 1
     },
     handleSelectDiary(id) {
+      this.bring = [
+        '軍手', '手袋', 'ジャケット', 'ハンカチ'
+      ]
       this.itinerary = [
         { type: '移動', duration: 30, description: '目安: 30分' },
         { type: '観光', duration: 30, description: 'しあわせ三蔵記念撮影(30分)' },
