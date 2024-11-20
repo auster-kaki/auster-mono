2240496288c14d615accad13cc73792a284717fe
soniccan <shunsuke.baba@so-tech.co.jp>
2024-11-21 03:40:31 +0900

    動きました。

--- backend/go/pkg/infrastructure/http/http.go
+++ backend/go/pkg/infrastructure/http/http.go
@@ -15,6 +15,8 @@ import (
 	appRPC "github.com/auster-kaki/auster-mono/pkg/app/rpc"
 )
 
+const imageRoot = "/assets"
+
 type Client struct {
 	client *http.Client
 	host   string
@@ -37,18 +39,21 @@ func (c *Client) URL() string {
 func (c *Client) Diary() appRPC.Diary { return c }
 
 func (c *Client) CreateImage(ctx context.Context, input appRPC.CreateImageInput) (appRPC.CreateImageOutput, error) {
-
+	var (
+		swapFacePath   = input.TargetPath
+		swapBeforePath = input.SourcePath
+	)
 	var buf bytes.Buffer
 	writer := multipart.NewWriter(&buf)
 
-	file, err := os.Open(input.SourcePath)
+	file, err := os.Open(swapFacePath)
 	if err != nil {
 		return appRPC.CreateImageOutput{}, err
 	}
 	defer file.Close()
 
 	// ファイルを追加
-	part, err := writer.CreateFormFile("source_path", filepath.Base(input.SourcePath))
+	part, err := writer.CreateFormFile("source_path", filepath.Base(swapFacePath))
 	if err != nil {
 		return appRPC.CreateImageOutput{}, err
 	}
@@ -58,7 +63,7 @@ func (c *Client) CreateImage(ctx context.Context, input appRPC.CreateImageInput)
 		return appRPC.CreateImageOutput{}, err
 	}
 
-	targetFile, err := os.Open(input.TargetPath)
+	targetFile, err := os.Open(swapBeforePath)
 	if err != nil {
 		return appRPC.CreateImageOutput{}, err
 	}
@@ -106,9 +111,61 @@ func (c *Client) CreateImage(ctx context.Context, input appRPC.CreateImageInput)
 }
 
 func (c *Client) GetStatus(ctx context.Context, input appRPC.GetStatusInput) (appRPC.GetStatusOutput, error) {
-	return appRPC.GetStatusOutput{}, nil
+	req, err := http.NewRequestWithContext(ctx, http.MethodGet, c.URL()+"/job_status/"+input.JobID, nil)
+	if err != nil {
+		return appRPC.GetStatusOutput{}, fmt.Errorf("failed to create request: %w", err)
+	}
+	res, err := c.client.Do(req)
+	if err != nil {
+		return appRPC.GetStatusOutput{}, fmt.Errorf("failed to do request: %w", err)
+	}
+	defer res.Body.Close()
+
+	var response struct {
+		Status string `json:"status"`
+	}
+
+	if err := json.NewDecoder(res.Body).Decode(&response); err != nil {
+		return appRPC.GetStatusOutput{}, fmt.Errorf("failed to decode response: %w", err)
+	}
+
+	return appRPC.GetStatusOutput{
+		// NOTE:クソみたいな仕様です....
+		JobID:  input.JobID,
+		Status: response.Status,
+	}, nil
 }
 
+// ファイル書き込みの設定をしてないので、未検証
 func (c *Client) GetImagePath(ctx context.Context, input appRPC.GetImagePathInput) (appRPC.GetImagePathOutput, error) {
-	return appRPC.GetImagePathOutput{}, nil
+
+	filename := fmt.Sprintf("%s.png", input.JobID)
+	url := fmt.Sprintf(c.URL() + "/generated/" + filename)
+
+	response, err := http.Get(url)
+	if err != nil {
+		return appRPC.GetImagePathOutput{}, fmt.Errorf("failed to get image: %w", err)
+	}
+	defer response.Body.Close()
+
+	// ステータスコードを確認
+	if response.StatusCode != http.StatusOK {
+		return appRPC.GetImagePathOutput{}, fmt.Errorf("server error: status code %d", response.StatusCode)
+	}
+
+	outFile, err := os.Create(filepath.Join(imageRoot, filename))
+	if err != nil {
+		return appRPC.GetImagePathOutput{}, fmt.Errorf("failed to create file: %w", err)
+	}
+	defer outFile.Close()
+
+	// レスポンスボディをファイルに書き込む
+	_, err = io.Copy(outFile, response.Body)
+	if err != nil {
+		return appRPC.GetImagePathOutput{}, fmt.Errorf("failed to write file: %w", err)
+	}
+
+	return appRPC.GetImagePathOutput{
+		ImageURL: filepath.Join(imageRoot, filename),
+	}, nil
 }
