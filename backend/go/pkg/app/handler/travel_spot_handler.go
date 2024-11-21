0216529dc55acef2f9b8d7ac46a6718ec4be7b76
shinnosuke-K <shinnosuke.kishida@so-tech.co.jp>
2024-11-22 02:35:28 +0900

    fix format

--- backend/go/pkg/app/handler/travel_spot_handler.go
+++ backend/go/pkg/app/handler/travel_spot_handler.go
@@ -3,8 +3,10 @@ package handler
 import (
 	"context"
 	"encoding/json"
+	"fmt"
 	"mime/multipart"
 	"net/http"
+	"time"
 
 	"github.com/auster-kaki/auster-mono/pkg/app/presenter/request"
 	"github.com/auster-kaki/auster-mono/pkg/app/presenter/response"
@@ -50,7 +52,7 @@ func (h *TravelSpotHandler) CreateDiary(w http.ResponseWriter, r *http.Request) 
 			response.HandleError(ctx, w, fmt.Errorf("failed to decode request body: %w", err))
 			return
 		}
-	
+
 		date, err := time.Parse(time.DateOnly, req.Date)
 		if err != nil {
 			response.HandleError(ctx, w, fmt.Errorf("failed to parse date: %w", err))
@@ -61,54 +63,55 @@ func (h *TravelSpotHandler) CreateDiary(w http.ResponseWriter, r *http.Request) 
 			response.HandleError(ctx, w, fmt.Errorf("failed to create diary: %w", err))
 			return
 		}
-	
-	// 画像ファイルとjsonを返す処理
-	w.Header().Set("Content-Type", "multipart/mixed; boundary=boundary")
 
-	// マルチパートレスポンスを生成
-	mw := multipart.NewWriter(w)
-	defer mw.Close()
+		// 画像ファイルとjsonを返す処理
+		w.Header().Set("Content-Type", "multipart/mixed; boundary=boundary")
 
-	// ユーザー情報をjsonで返す
-	j, err := json.Marshal(map[string]any{
-		"ID":    out.ID,
-		"Title": out.Title,
-		"Body":  out.Body,
-	})
-	if err != nil {
-		response.HandleError(r.Context(), w, err)
-		return
-	}
-	// ユーザー情報をマルチパートレスポンスに書き込む
-	p, err := mw.CreatePart(map[string][]string{
-		"Content-Type": {"application/json"},
-	})
-	if err != nil {
-		response.HandleError(r.Context(), w, err)
-		return
-	}
-	if _, err := p.Write(j); err != nil {
-		response.HandleError(r.Context(), w, err)
-		return
-	}
+		// マルチパートレスポンスを生成
+		mw := multipart.NewWriter(w)
+		defer mw.Close()
 
-	// 画像をマルチパートレスポンスに書き込む
-	imagePart, err := mw.CreatePart(map[string][]string{
-		"Content-Type": {"image/png"},
-	})
-	if err != nil {
-		response.HandleError(r.Context(), w, err)
-		return
-	}
-	if _, err := imagePart.Write(out.Photo); err != nil {
-		response.HandleError(r.Context(), w, err)
-		return
-	}
+		// ユーザー情報をjsonで返す
+		j, err := json.Marshal(map[string]any{
+			"ID":    out.ID,
+			"Title": out.Title,
+			"Body":  out.Body,
+		})
+		if err != nil {
+			response.HandleError(r.Context(), w, err)
+			return
+		}
+		// ユーザー情報をマルチパートレスポンスに書き込む
+		p, err := mw.CreatePart(map[string][]string{
+			"Content-Type": {"application/json"},
+		})
+		if err != nil {
+			response.HandleError(r.Context(), w, err)
+			return
+		}
+		if _, err := p.Write(j); err != nil {
+			response.HandleError(r.Context(), w, err)
+			return
+		}
 
-	// マルチパートレスポンスを閉じる
-	if err := mw.Close(); err != nil {
-		response.HandleError(r.Context(), w, err)
-		return
+		// 画像をマルチパートレスポンスに書き込む
+		imagePart, err := mw.CreatePart(map[string][]string{
+			"Content-Type": {"image/png"},
+		})
+		if err != nil {
+			response.HandleError(r.Context(), w, err)
+			return
+		}
+		if _, err := imagePart.Write(out.Photo); err != nil {
+			response.HandleError(r.Context(), w, err)
+			return
+		}
+
+		// マルチパートレスポンスを閉じる
+		if err := mw.Close(); err != nil {
+			response.HandleError(r.Context(), w, err)
+			return
+		}
 	}
 }
 
