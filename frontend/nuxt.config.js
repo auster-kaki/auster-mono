558d920629f0eea51647a0e5a118495d2f7dc997
shimoe-sot <mamoru.shimoe@so-tech.co.jp>
2024-11-22 16:02:21 +0900

    PWA追加してみる

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -19,9 +19,14 @@ export default {
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' }
+      { name: 'format-detection', content: 'telephone=no' },
+      { name: 'apple-mobile-web-app-capable', content: 'yes' },// for pwa
+      { name: 'apple-mobile-web-app-status-bar-style', content: 'black-translucent' } // for pwa
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'apple-touch-icon', href: '/icon.png' } // iOS用アイコン
+    ]
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
@@ -45,8 +50,24 @@ export default {
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
-    '@nuxtjs/dotenv'
+    '@nuxtjs/dotenv',
+    '@nuxtjs/pwa'
   ],
+  pwa: {
+    manifest: {
+      name: 'My PWA App', // アプリの名前
+      short_name: 'MyApp', // アイコン下に表示される短縮名
+      description: 'A Progressive Web App built with Nuxt.js',
+      display: 'standalone', // フルスクリーンモードを指定
+      start_url: '/', // 起動時のURL
+      background_color: '#ffffff', // スプラッシュスクリーンの背景色
+      theme_color: '#3CB371', // ステータスバーやナビゲーションバーの色
+      lang: 'ja' // アプリの言語設定
+    },
+    workbox: {
+      enabled: true // 開発モードでも有効にする場合は true
+    }
+  },
   env: {
     baseURL: process.env.BASE_URL || 'http://localhost:8080'
   },
